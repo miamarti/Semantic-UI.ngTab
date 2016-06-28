e59cce726a43f62d6123f016a3b66e2edf7c6a48
Miller Augusto <miller.augusto@gmail.com>
2016-06-28 17:14:03 -0300

    Update ngTab.js

--- dist/ngTab.js
+++ dist/ngTab.js
@@ -1,6 +1,6 @@
 (function () {
     'use strict';
-    (angular.module('ngSidebar', ['ng'])).directive('ngTab', function () {
+    (angular.module('ngTab', ['ng'])).directive('ngTab', function () {
         return {
             restrict: 'A',
             link: function (scope, elem) {
